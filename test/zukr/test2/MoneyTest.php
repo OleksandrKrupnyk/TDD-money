c082619a24a153d6f1f100ff96a21e486e8ce2ea
Alex Krupnik <krupnik_a@ukr.net>
2019-07-21 03:11:32 +0300

    chapter 11

--- test/zukr/test2/MoneyTest.php
+++ test/zukr/test2/MoneyTest.php
@@ -9,7 +9,7 @@ namespace zukr\test2;
 
 use PHPUnit\Framework\TestCase;
 
-class DollarTest extends TestCase
+class MoneyTest extends TestCase
 {
 
     public function testTimes(): void
@@ -23,8 +23,6 @@ class DollarTest extends TestCase
     {
         self::assertTrue(Money::dollar(5)->equals(Money::dollar(5)));
         self::assertFalse(Money::dollar(5)->equals(Money::dollar(6)));
-        self::assertTrue(Money::franc(5)->equals(Money::franc(5)));
-        self::assertFalse(Money::franc(5)->equals(Money::franc(6)));
         self::assertFalse(Money::franc(5)->equals(Money::dollar(5)), 'Franc 5 != Dollar 5');
     }
 
